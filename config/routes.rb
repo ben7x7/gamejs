fe93e9f013744cdac90c82e5c751d9e750077eec
benoit bessiere <benoit.bessiere@gmail.com>
2020-04-23 12:45:50 +0200

    memory html + css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'jackpot', to: 'pages#jackpot', as: :jackpot
+  get 'memory', to: 'pages#memory', as: :memory
   # get 'projects', to: 'pages#projects', as: :projects
   # get 'contact', to: 'pages#contact', as: :contact
 end
