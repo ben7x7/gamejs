d97626f85a5665e55c431706ba0a915c260a1c15
benoit bessiere <benoit.bessiere@gmail.com>
2020-05-11 01:19:32 +0200

    update colors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 import 'bootstrap';
 
-import { navSlide } from '../components/navbar.js';
-navSlide();
-
 import  { playJackpot } from '../components/jackpot.js';
+
+if (document.readyState === 'loading'){
+  document.addEventListener('DOMContentLoaded', playJackpot());
+}
 playJackpot();
+
+
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'jackpot', to: 'pages#jackpot', as: :jackpot
-
-  # get 'contact', to: 'pages#contact', as: :contact
 end
