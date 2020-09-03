3a0aa819e89ee39d1c50765829b141ecd28adb61
benoit bessiere <benoit.bessiere@gmail.com>
2020-09-03 15:47:59 +0200

    change src to cloudinary

--- app/javascript/components/jackpot.js
+++ app/javascript/components/jackpot.js
@@ -20,77 +20,77 @@ const playJackpot = () => {
     let el = document.getElementById('btn-roll');
     if(el){
       el.addEventListener('click', () => {
-      // random dices
-      dice1 = Math.floor(Math.random()*6)+1;
-      dice2 = Math.floor(Math.random()*6)+1;
-      totalDices = dice1 + dice2;
-      clicked = false;
-      // display dices results
-      document.getElementById('dice1').src = "http://jackpotjs.herokuapp/app/assets/images/dice-" + dice1 + ".png";
-      document.getElementById('dice2').src = "http://jackpotjs.herokuapp/app/assets/images/dice-" + dice2 + ".png";
-      // disabled btn-roll
-      document.getElementById('btn-roll').setAttribute('disabled', '');
+        // random dices
+        dice1 = Math.floor(Math.random()*6)+1;
+        dice2 = Math.floor(Math.random()*6)+1;
+        totalDices = dice1 + dice2;
+        clicked = false;
+        // display dices results
+        document.getElementById('dice1').src = 'https://res.cloudinary.com/bessiere/image/upload/v1599140242/dice-' + dice1 + '.png';
+        document.getElementById('dice2').src = 'https://res.cloudinary.com/bessiere/image/upload/v1599140242/dice-' + dice2 + '.png';
+        // disabled btn-roll
+        document.getElementById('btn-roll').setAttribute('disabled', '');
 
-      let d1 = 0;
-      let d2 = 0;
-      let totalD = 0;
+        let d1 = 0;
+        let d2 = 0;
+        let totalD = 0;
 
-      if (!flippedTiles.includes(dice1)) {
-        d1 = dice1;
-      } else {
-        d1 = 0;
-      }
+        if (!flippedTiles.includes(dice1)) {
+          d1 = dice1;
+        } else {
+          d1 = 0;
+        }
 
-      if (!flippedTiles.includes(dice2)) {
-        d2 = dice2;
-      } else {
-        d2 = 0;
-      }
+        if (!flippedTiles.includes(dice2)) {
+          d2 = dice2;
+        } else {
+          d2 = 0;
+        }
 
-      if (!flippedTiles.includes(totalDices) && totalDices <= 9) {
-        totalD = totalDices;
-      } else {
-        totalD = 0;
-      }
-      console.log(d1, d2, totalD)
+        if (!flippedTiles.includes(totalDices) && totalDices <= 9) {
+          totalD = totalDices;
+        } else {
+          totalD = 0;
+        }
+        console.log(d1, d2, totalD)
 
-      if (d1 === 0 && d2 === 0 && totalD === 0){
-        setTimeout(() => {
-          gameOver();
-        },1500);
-      }
+        if (d1 === 0 && d2 === 0 && totalD === 0){
+          setTimeout(() => {
+            gameOver();
+          },1500);
+        }
 
 
-      // condition to allow the flip
-      const canFlipTile = () => {
-        return clicked === false;
-      };
+        // condition to allow the flip
+        const canFlipTile = () => {
+          return clicked === false;
+        };
 
-      // victory function
-      const victory = () => {
-        reset();
-        document.getElementById('victory-text').classList.add('visible');
-      };
-      // flipTile function
-      const flipTile = tile => {
-        tiles.forEach(tile => {
-          // Event listener for each tiles
-          tile.addEventListener('click', function(){
-            let tileVal = parseInt(this.dataset.value)
-            if(canFlipTile() && (tileVal === d1 || tileVal === d2 || tileVal === totalD)) {
-              tile.classList.add('flipped');
-              flippedTiles.push(tileVal);
-              clicked = true;
-              document.getElementById('btn-roll').removeAttribute('disabled', '');
-              console.log(flippedTiles);
-              if (flippedTiles.length === tiles.length)
-                victory();
-            }
+        // victory function
+        const victory = () => {
+          reset();
+          document.getElementById('victory-text').classList.add('visible');
+        };
+        // flipTile function
+        const flipTile = tile => {
+          tiles.forEach(tile => {
+            // Event listener for each tiles
+            tile.addEventListener('click', function(){
+              let tileVal = parseInt(this.dataset.value)
+              if(canFlipTile() && (tileVal === d1 || tileVal === d2 || tileVal === totalD)) {
+                tile.classList.add('flipped');
+                flippedTiles.push(tileVal);
+                clicked = true;
+                document.getElementById('btn-roll').removeAttribute('disabled', '');
+                console.log(flippedTiles);
+                if (flippedTiles.length === tiles.length)
+                  victory();
+              }
+            });
           });
-        });
-      };
-      flipTile();
-    });
+        };
+        flipTile();
+      });
     }
 
     // Restart conditions
@@ -104,8 +104,8 @@ const playJackpot = () => {
       // set clicked to true to click nowhere
       clicked = true;
       // reset dice1 and dice2 view
-      document.getElementById('dice1').src = 'htttp://jackpotjs.herokuapp/app/assets/images/dice-1.png';
-      document.getElementById('dice2').src = 'htttp://jackpotjs.herokuapp/app/assets/images/dice-1.png';
+      document.getElementById('dice1').src = 'https://res.cloudinary.com/bessiere/image/upload/v1599140242/dice-6.png';
+      document.getElementById('dice2').src = 'https://res.cloudinary.com/bessiere/image/upload/v1599140242/dice-6.png';
       // Reset Roll button
       document.getElementById('btn-roll').removeAttribute('disabled', '');
     }
